1625151434f517e3d3cf9f7ecec39a4c1e5dc48f
you@example.com <you@example.com>
2025-05-07 14:17:51 -0500

    se crea cambio

---
+++ src/components/pages/Home/Home.jsx
@@ -0,0 +1,18 @@
+import './Home.css'
+export function Home () {
+    return (
+        <>
+        <section className="container">
+            <div className="row justify-content-center">
+                <div className="col-4">
+                    <h1>ALEXANDRA DUQUE LOPEZ</h1>
+                    <img src="../../../../src/assets/img/img1.jpg" className="img-fluid rounded shadow foto" />
+                    <hr />
+                    <p>Soy una estudiante de Desarrollo de Software apasionada por la tecnología y el aprendizaje constante. Me encanta descubrir nuevas historias a través de la lectura, ya que encuentro en los libros una forma de expandir mi creatividad y conocimiento. La música también ocupa un lugar especial en mi vida, acompañándome en diferentes momentos y motivándome en mi día a día. Disfruto salir, explorar nuevos lugares y vivir nuevas experiencias, especialmente cuando se trata de probar comidas diferentes y descubrir sabores únicos. Soy una persona curiosa, abierta a nuevas ideas y siempre en búsqueda de crecer tanto personal como profesionalmente.
+                    Me considero una persona responsable, comprometida y con una gran capacidad de adaptación. Estoy en constante búsqueda de nuevos desafíos que me permitan seguir aprendiendo y creciendo en mi carrera profesional.</p>
+                </div>
+            </div>
+        </section>
+        </>
+    )
+}
---
+++ src/components/pages/Introduccion/Introduccion.jsx
@@ -0,0 +1,64 @@
+export function Introduccion () {
+    return (
+        <> 
+       < section className="container">
+            <div className="row">
+                <div className="col-12 col-md-7">
+                    <img src="../../../../src/assets/img/imagenanimada.png"
+                     className="img-fluid rounded" />
+                </div>
+                <div className="col-12 col-md-5">
+                    <h1 className="display-1">Alexandra Duque Lopez &copy;</h1>
+                    <p>I am a Software Development student based in Medellín, Colombia. I have developed strong foundational skills in programming logic, SQL database design, and front-end development using JavaScript, HTML, and CSS. Additionally, I have experience working with React for building dynamic user interfaces and have a good understanding of Agile methodologies. I also have knowledge of Java, allowing me to work on object-oriented programming projects and backend development tasks.
+
+                    I am motivated to continue learning and growing within the field of software development, with a particular interest in building scalable web applications and contributing to collaborative development environments.</p>
+
+                    <div className="row">
+                        <div className="col-4">
+                            <div className="card shadow w-100">
+                                <img src="../../../../src/assets/img/css-3.png"  className="img-fluid" />
+                            </div>
+                        </div>
+                        <div className="col-4">
+                            <div className="card shadow w-100">
+                                <img src="../../../../src/assets/img/html-5.png"  className="img-fluid" />
+                            </div>
+                        </div>
+                        <div className="col-4">
+                            <div className="card shadow w-100">
+                                <img src="../../../../src/assets/img/java.png"  className="img-fluid" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="row mt-4">
+                        <div className="col-4">
+                            <div className="card shadow w-100">
+                                <img src="../../../../src/assets/img/js.png"  className="img-fluid" />
+                            </div>
+                        </div>
+                        <div className="col-4">
+                            <div className="card shadow w-100">
+                                <img src="../../../../src/assets/img/physics.png"  className="img-fluid" />
+                            </div>
+                        </div>
+                        <div className="col-4">
+                            <div className="card shadow w-100">
+                                <img src="../../../../src/assets/img/piton.png"  className="img-fluid" />
+                            </div>
+                        </div>
+                    </div>
+                    
+                       
+                    
+                    
+                    
+                    
+                        
+                    
+                </div>
+            </div>
+       </section>
+        </>
+    )
+}
