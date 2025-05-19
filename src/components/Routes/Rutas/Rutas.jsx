f47ecbf2da6516d4e60cbd1212e1289562692de2
you@example.com <you@example.com>
2025-05-19 14:06:09 -0500

    se crea cambio final

--- src/components/Routes/Rutas/Rutas.jsx
+++ src/components/Routes/Rutas/Rutas.jsx
@@ -5,10 +5,14 @@ import {Historia} from "../../pages/Historia/Historia";
 import {Introduccion} from "../../pages/Introduccion/Introduccion";
 import {Experiencia} from "../../pages/Experiencia/Experiencia";
 
+import {Menu} from "../../common/Menu/Menu";
+
 export function Rutas() {
     return (
         <>
+        <Menu></Menu>
         <Routes>
+            <Route path='/' element={<Introduccion/>}></Route>
             <Route path='/inicio' element={<Home/>}></Route>
             <Route path='/proyectos' element={<Historia/>}></Route>
             <Route path='/Alexandra' element={<Introduccion/>}></Route>
